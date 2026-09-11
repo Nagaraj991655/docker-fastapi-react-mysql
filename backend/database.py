92f99d5f8b7359d1c92c88b4259190c3e0f16870
Ubuntu <ubuntu@ip-172-31-87-246.ec2.internal>
2026-09-11 07:19:08 +0000

    Testing CI/CD

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+pymysql://root:abhinavfu@localhost:3306/employee_db')
+DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+pymysql://root:abhinavfu@mysql-container:3306/employee_db')
 # DATABASE_URL = "mysql+mysqlconnector://root:example@db:3306/mydatabase"
 
 engine = create_engine(DATABASE_URL)
